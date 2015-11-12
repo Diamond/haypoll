1ad4a5510ccec14f77d19394fbd4b6c5018f7666
Brandon Richey <brichey@teladoc.com>
2015-11-12 17:28:28 -0500

    Cleaned up the javascript file a bit

--- web/static/js/live_poller.js
+++ web/static/js/live_poller.js
@@ -6,56 +6,56 @@ export var LivePoller = {
       return;
     }
     console.log("LivePoller init")
-    let socket = this.createSocket()
-    let pollId = 7
-    let pollChannel = socket.channel("polls:" + pollId, {})
+    let pollChannel = this.setupPollChannel()
+    this.setupVoteButtons(pollChannel)
+  },
+  createSocket: function() {
+    let socket = new Socket("/socket", {params: { token: window.userToken }})
+    socket.connect()
+    socket.onOpen(() => console.log("Connected"))
+    return socket
+  },
+  setupPollChannel: function() {
     let self = this
-    pollChannel.on("new_vote", vote => {
-      console.log("Received new vote: ", vote)
-        console.log("Entry Id: ", vote["entry_id"])
-      self.updateDisplay(vote["entry_id"])
-    })
+    let socket = this.createSocket()
+    let pollId = $("#poll-id").val()
+    let pollChannel = socket.channel("polls:" + pollId, {})
+    pollChannel.on("new_vote", vote => { self.updateDisplay(vote["entry_id"]) })
     pollChannel.join()
-      .receive("ok", resp => {
-        console.log("Joined")
-      })
+      .receive("ok", resp => { console.log("Joined") })
       .receive("error", reason => console.log("Error: ", reason))
+    return pollChannel
+  },
+  setupVoteButtons: function(pollChannel) {
     $(".vote").on("click", function(event) {
       event.preventDefault()
       let entry_id = $(event.currentTarget).data("entry-id")
       pollChannel.push("new_vote", { entry_id: entry_id })
     })
   },
-  createSocket: function() {
-    let socket = new Socket("/socket",
-      {
-        logger: (kind, msg, data) => {
-          console.log(`${kind}: ${msg}`, data)
-        },
-        params: { token: window.userToken }
-      }
-    )
-    socket.connect()
-    socket.onOpen(() => console.log("Connected"))
-    return socket
-  },
-  updateDisplay: function(entry_id) {
-    let total = (+$("#total-entries").val() + 1)
-    $("#total-entries").val(total)
+  updateDisplay: function(entryId) {
+    let total = this.updateTotal()
+    let self = this
     $("li.entry").each(function() {
       let li = $(this)
-      if (entry_id == li.data("entry-id")) {
-        let new_votes = +(li.data("entry-votes")) + 1
-        let percent = ((new_votes / total) * 100).toFixed(2)
-        console.log(total, new_votes, percent)
-        li.find(".score").text(new_votes + " votes (" + percent + "%)" )
-        li.data("entry-votes", new_votes)
+      if (entryId == li.data("entry-id")) {
+        let newVotes = li.data("entry-votes") + 1
+        self.updateEntry(li, newVotes, total)
       } else {
-        let new_votes = li.data("entry-votes")
-        let percent = ((new_votes / total) * 100).toFixed(2)
-        console.log(total, new_votes, percent)
-        li.find(".score").text(new_votes + " votes (" + percent + "%)" )
+        let newVotes = li.data("entry-votes")
+        self.updateEntry(li, newVotes, total)
       }
     })
+  },
+  updateTotal: function() {
+    let total = (+$("#total-entries").val() + 1)
+    $("#total-entries").val(total)
+    return total
+  },
+  updateEntry: function(li, newVotes, total) {
+    let percent = ((newVotes / total) * 100).toFixed(2)
+    li.find(".score").text(newVotes + " votes (" + percent + "%)" )
+    li.data("entry-votes", newVotes)
   }
+
 }
